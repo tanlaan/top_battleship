11c17433709b629a68a6b194fe8b85c0992cbc53
Chris Bolas <tanlaan33@gmail.com>
2020-12-26 10:31:05 -0800

    Added isValidPlay function and test

--- src/game/GameBoard.js
+++ src/game/GameBoard.js
@@ -2,26 +2,54 @@ import Ship from './Ship'
 
 const GameBoard = () =>{
     const placeShip = (playBoard, ships, length, coordinate, orientation='H') => {
+        const x = coordinate.slice(0, 1)
+        const y = coordinate.slice(1)
         const newShip = Ship(length)
+        const positions = {}
+
+        // Is our entire ship on the game board?
+        if (!isValidMove(x, y, length)) return false
+
+        // Calculate all the coordinates it will take up
         for ( let i = 0; i < length; i++ ) {
-            const x = coordinate.slice(0, 1)
-            const y = coordinate.slice(1)
             if (orientation === 'H') {
 
                 // We are increasing horizontally
-                playBoard[x + (Number(y) + i)] = () => newShip.hit(i)
+                positions[x + (Number(y) + i)] = () => newShip.hit(i)
             } else {
 
                 // We are increasing vertically
                 // So coerce a new letter
                 const newX = String.fromCharCode(x.charCodeAt(0) + i )
-                playBoard[newX + y] = () => newShip.hit(i)
+                positions[newX + y] = () => newShip.hit(i)
             }
         }
+        if (!isValidPlay(positions, playBoard)) return false 
+
+        // Integrate new ship positions into playBoard
+        Object.assign(playBoard, positions)
         ships.push(newShip)
         // Success!
         return true
     }
+
+    const isValidMove = (x, y, length) => {
+        // Check coordinate out of bounds
+        const xBoundary = String.fromCharCode('J'.charCodeAt(0) - length)
+        const yBoundary = 10 - length
+        if (x < 'A' || x > xBoundary) return false
+        if (y < 1 || y > yBoundary) return false
+        return true
+    }
+
+    const isValidPlay= (positions, playBoard) => {
+        let keys = Object.keys(positions)
+        for (let i = 0; i < keys.length; i++) {
+            if (keys[i] in playBoard) return false
+        }
+        return true
+    }
+
     const playerPlayBoard = {}
     const computerPlayBoard = {}
     const playerMoveBoard = initBoard()
--- src/game/GameBoard.test.js
+++ src/game/GameBoard.test.js
@@ -1,57 +1,78 @@
 import GameBoard from './GameBoard'
 import Ship from './Ship'
 
-const board = GameBoard()
-
 test('GameBoard factory function is defined', () => {
     expect(GameBoard).toBeDefined()
 })
 
 test('A GameBoard object has a function called placeShip', () => {
+    const board = GameBoard()
     expect(board.placeShip).toBeDefined()
 })
 
 test('A GameBoard object has a playBoard for player and computer', () => {
+    const board = GameBoard()
     expect(board.playerPlayBoard).toBeDefined()
     expect(board.computerPlayBoard).toBeDefined()
 })
 
 test('A GameBoard object has a move board for player and computer', () => {
+    const board = GameBoard()
     expect(board.playerMoveBoard).toBeDefined()
     expect(board.computerMoveBoard).toBeDefined()
 })
 
 test('moveBoards will be initialized with 10 columns', () => {
+    const board = GameBoard()
     expect(board.playerMoveBoard.length).toBe(10)
     expect(board.computerMoveBoard.length).toBe(10)
 })
 
 test('moveBoards will be initialized with 10 rows', () => {
+    const board = GameBoard()
     expect(board.playerMoveBoard[0].length).toBe(10)
     expect(board.computerMoveBoard[0].length).toBe(10)
 })
 
 test('There should be a location to place the ships associated with a player and computer', () => {
+    const board = GameBoard()
     expect(board.playerShips).toBeDefined()
     expect(board.computerShips).toBeDefined()
 })
 
 test('Gameboard can place a ship onto a playboard', () => {
+    const board = GameBoard()
     board.placeShip(board.playerPlayBoard, board.playerShips, 3, 'A1')
     expect(board.playerPlayBoard['A1']).toBeDefined()
 })
 
 test('Placing a ship on a playBoard adds it to the ships array', () => {
+    const board = GameBoard()
     board.placeShip(board.playerPlayBoard, board.playerShips, 3, 'A1')
     expect(board.playerShips[0]).toBeDefined()
 })
 
 test("Placing a ship on a playBoard adds more than just the initial coordinate", () => {
+    const board = GameBoard()
     board.placeShip(board.playerPlayBoard, board.playerShips, 3, 'A1')
     expect(board.playerPlayBoard['A2']).toBeDefined()
 })
 
 test("Placing a ship on the playBoard works in the vertical orientation as well", () => {
+    const board = GameBoard()
     board.placeShip(board.playerPlayBoard, board.playerShips, 3, 'A1', 'V')
     expect(board.playerPlayBoard['B1']).toBeDefined()
+})
+
+test("Placing a ship on the playBoard in an invalid location returns false", () => {
+    const board = GameBoard()
+    let value = board.placeShip(board.playerPlayBoard, board.playerShips, 3, 'K1', 'V')
+    expect(value).toBeFalsy()
+})
+
+test('Placeing a ship where it would overlap another returns false', () => {
+    const board = GameBoard()
+    board.placeShip(board.playerPlayBoard, board.playerShips, 3, 'A1')
+    let value = board.placeShip(board.playerPlayBoard, board.playerShips, 3, 'A1')
+    expect(value).toBeFalsy()
 })
